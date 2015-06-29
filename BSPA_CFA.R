9bfe63a67b6aa76ea35fa04e5a2d311a7ea7cf52
RegisBK <regiskomperda@gmail.com>
2015-06-29 18:31:07 -0400

    Added correction for non normality

--- BSPA_CFA.R
+++ BSPA_CFA.R
@@ -22,8 +22,14 @@ BSPA =~ Q1 + Q2 + Q3 + Q4 + Q5 + Q6 + Q7 + Q8 + Q9 + Q10 + Q11 + Q12 + Q13
 '
 ## This code actually runs the CFA and chooses to standardize the factors instead of fixing a loading to 1
 BSPAfit <- cfa(BSPAmodel, data = BSPA, std.lv=TRUE)
-
 summary(BSPAfit, fit.measures = TRUE)
+
+## Added the Satorra-Bentler correction since data are dichotomous instead of normal continuous -RK
+BSPAfitSB <- cfa(BSPAmodel, data = BSPA, std.lv=TRUE, estimator="MLM", test = "Satorra.Bentler")
+summary(BSPAfitSB, fit.measures = TRUE)
+
+## Summary and fit measures seem the same with and without the correction
+
 
 ## Plotting
 library(semPlot)
